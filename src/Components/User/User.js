55864b78dfafae97956fbcb0b41dec660be2c80f
Vincent Man <vincentman2702@hotmail.com>
2019-10-17 18:07:35 +0100

    initial commit

---
+++ src/Components/User/User.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './User.scss';
+
+function capitalise1stLetter(string) {
+    return string.charAt(0).toUpperCase() +
+        string.substr(1).toLowerCase()
+}
+
+function removeChar(string) {
+    return string
+        .substr(0, 10)
+        .split('-')
+        .reverse()
+        .join('-');
+}
+
+function User({ picture, firstname, lastname, gender, city, postcode, email, dob, phone, country }) {
+    return (
+        <article className='User'>
+            <img src={picture} alt={lastname} />
+            <h3>First Name: {capitalise1stLetter(firstname)}</h3>
+            <h3>Last Name: {capitalise1stLetter(lastname)}</h3>
+            <h3>Gender: {capitalise1stLetter(gender)}</h3>
+            <h3>City: {capitalise1stLetter(city)}</h3>
+            <h3>Postcode: {postcode}</h3>
+            <h3>Email: {email}</h3>
+            <h3>DOB: {removeChar(dob)}</h3>
+            <h3>Phone: {phone.split('-')}</h3>
+            <h3>Country: {country}</h3>
+        </article>
+    );
+}
+
+export default User; 
